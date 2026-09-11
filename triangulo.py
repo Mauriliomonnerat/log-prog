91359474a735e50ec26546835d2390dfb9bde92f
Maurilio Kachan Monnerat Pinho <luz0057916@aluno.firjansenaisesi.com.br>
2026-09-11 19:44:09 -0300

    Versão que não pega

--- triangulo.py
+++ triangulo.py
@@ -1,8 +1,8 @@
-lado1 = int(input("Digite o primeiro lado do triângulo: "))
-lado2 = int(input("Digite o segundo lado do triângulo: "))
-lado3 = int(input("Digite o terceiro lado do triângulo: "))
-
-condiexis = (lado1 + lado2 > lado3) and (lado1 + lado3 > lado2) and (lado2 + lado3 > lado1)
+lado1 = float(input("Digite o primeiro lado do triângulo: "))
+lado2 = float(input("Digite o segundo lado do triângulo: "))
+lado3 = float(input("Digite o terceiro lado do triângulo: "))
+#versão que não pega
+condiexis = (lado1 + lado2 > lado3) or (lado1 + lado3 > lado2) or (lado2 + lado3 > lado1)
 
 if condiexis:
     pass
