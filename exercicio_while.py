d5f7e5fef9ad6771e7d560e0be341e0d3abc62ee
Maurilio Kachan Monnerat Pinho <luz0057916@aluno.firjansenaisesi.com.br>
2026-09-14 21:05:32 -0300

    Exercício com while

---
+++ exercicio_while.py
@@ -0,0 +1,14 @@
+i = 0  #começa no 0
+
+while i < 10:       #faça enquanto i < 10
+    print(i)        #mostre o valor de i
+    # i = i + 1
+    i += 1          #soma i + 1
+
+
+i = 0  #começa no 0
+
+while i >= 0:       #faça enquanto i >= 0
+    print(i)        #mostre o valor de i
+    # i = i + 1
+    i -= 1          #subtrai i - 1
